e8583e216207758b2d22ae4d53a6558a1c8fb28c
Matias Olivera <matibjolivera@gmail.com>
2021-02-06 03:37:09 -0300

    delete params parameter

--- app/controllers/mercado_pago_controller.rb
+++ app/controllers/mercado_pago_controller.rb
@@ -1,7 +1,7 @@
 class MercadoPagoController < ApplicationController
   BASE_URL = 'https://api.mercadopago.com/v1/'
 
-  def get(resource, params = null)
+  def get(resource)
     JSON.parse(RestClient.get(resource, {:Authorization => 'Bearer ' + ENV['ACCESS_TOKEN']}))
   end
 end
