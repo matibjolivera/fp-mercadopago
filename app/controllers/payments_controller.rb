d736091726ea3104f41ba6737a6ae3f9cc4e12ad
Matias Olivera <matibjolivera@gmail.com>
2021-02-06 03:13:13 -0300

    try index method

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,5 +1,9 @@
 class PaymentsController < MercadoPagoController
   def show
+    puts "entré a método show"
     puts RestClient.get(URL + params.id + '?access_token=' + ENV['ACCESS_TOKEN'])
+  end
+  def index
+    puts "index"
   end
 end
